ed0f7b294aa943b67d48cf071d29f9ee0e879087
Aulas de Informática F.C.E <aulasfce4@udistrital.edu.co>
2019-11-13 07:52:07 -0500

    act

--- 18_Nov_13/caida_libre.m
+++ 18_Nov_13/caida_libre.m
@@ -1,15 +1,21 @@
 clc;clear;
 dt = .01;
-t = 0:dt:1;
-
-vo = 0;     h = 0;  g = -9.8;
-plot(0,h,'ro'); grid; axis([-2 2, -600 5]);
+t = 0:dt:1.5;
+piso = -200;
+vo = 250;     h = 0;  g = -9.8;
+plot(0,h,'ro'); grid; axis([-.2 5, -600 250]);
 hold on;
 
 for i = 1:length(t)
     vf = vo+g*t(i);
     h = vo*t(i)+.5*g*t(i)*t(i);
-    plot(0,h,'ro');
+    if h < piso
+        vo = abs(vf);
+        %h = vo*t(i)+.5*g*t(i)*t(i);
+        continue
+    end
+    plot(t(i),h,'ro');
+    %text(t(i),h,'o');
     vo = vf;
     pause(.1);
 end
