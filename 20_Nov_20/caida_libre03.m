cb5aa0e316bd367429ba9494c3f6a27cf1959d09
Aulas de Informática F.C.E <aulasfce4@udistrital.edu.co>
2019-11-20 07:25:03 -0500

    Nov 20

--- 20_Nov_20/caida_libre03.m
+++ 20_Nov_20/caida_libre03.m
@@ -1,6 +1,6 @@
 clc;clear;
 dt = .1;
-t = 0:dt:40;
+t = 0:dt:50;
 piso = -300;
 vo = 50;     ho = 0;  g = -9.8;
 b  = .8;
@@ -13,6 +13,12 @@ hold on;
 for i = 1:length(t)
     hf = ho+vo*dt+1/2*g*dt^2;
     vf = vo+g*dt;
+    
+    if t(i)>20 & t(i)<30
+        piso = -200;
+    elseif t(i)>30
+        piso = -400;
+    end
     
     if hf < piso
         vf = abs(vf)*b;
@@ -29,3 +35,8 @@ end
 % x = x0+vx*t
 % vy = v0y+ay*t
 % y = y0+v0y*t+1/2*ay*t^2
+
+% PENDIENTES:
+% Explorar temáticas para desarrollar en simulación:
+% Fenómeno físico con interactividad (con el usuario).  Posibilidades
+% Cargas eléctricas, péndulo (invertido), colisiones ...
